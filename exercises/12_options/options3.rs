f24be27611d6bb27204a3f7a4737478d2e9982b3
nico-vrn <networksgamesni@gmail.com>
2024-04-23 16:11:49 +0200

    rustdocs

--- exercises/12_options/options3.rs
+++ exercises/12_options/options3.rs
@@ -3,15 +3,19 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
+//! Ce programme définit une structure Point représentant des coordonnées x et y, puis utilise une option pour stocker un point ou aucune valeur. Ensuite, il utilise un motif `if let` pour extraire les coordonnées du point s'il est présent.
 
+/// Structure représentant des coordonnées x et y.
 struct Point {
     x: i32,
     y: i32,
 }
 
 fn main() {
+    /// Option contenant un point
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
+    /// Utilisation d'un motif `if let` pour extraire les coordonnées du point
     if let Some(ref p) = y {
         println!("Co-ordinates are {},{} ", p.x, p.y);
     } else {
