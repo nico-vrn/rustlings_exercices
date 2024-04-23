f8aa3ba8b43deea644d0ce60633fb8d8226e25d4
nico-vrn <networksgamesni@gmail.com>
2024-04-23 16:16:37 +0200

    rustdocs

--- exercises/17_tests/tests4.rs
+++ exercises/17_tests/tests4.rs
@@ -5,13 +5,17 @@
 // Execute `rustlings hint tests4` or use the `hint` watch subcommand for a
 // hint.
 
+//! Ce module définit une structure `Rectangle` représentant un rectangle avec une largeur et une hauteur.
+//! Les fonctions de test vérifient le comportement de la méthode `new` dans différentes situations.
+
+/// Représente un rectangle avec une largeur et une hauteur.
 struct Rectangle {
     width: i32,
     height: i32
 }
 
 impl Rectangle {
-    // Only change the test functions themselves
+    /// Crée un nouveau rectangle avec la largeur et la hauteur spécifiées.
     pub fn new(width: i32, height: i32) -> Self {
         if width <= 0 || height <= 0 {
             panic!("Rectangle width and height cannot be negative!")
@@ -26,7 +30,7 @@ mod tests {
 
     #[test]
     fn correct_width_and_height() {
-        // This test should check if the rectangle is the size that we pass into its constructor
+        // Vérifie si le rectangle a la largeur et la hauteur spécifiées dans le constructeur.
         let rect = Rectangle::new(10, 20);
         assert_eq!(rect.width, 10); // check width
         assert_eq!(rect.height, 20); // check height
@@ -35,14 +39,14 @@ mod tests {
     #[test]
     #[should_panic]
     fn negative_width() {
-        // This test should check if program panics when we try to create rectangle with negative width
+        // Vérifie si le programme panique lors de la tentative de création d'un rectangle avec une largeur négative.
         let _rect = Rectangle::new(-10, 10);
     }
 
     #[test]
     #[should_panic]
     fn negative_height() {
-        // This test should check if program panics when we try to create rectangle with negative height
+        // Vérifie si le programme panique lors de la tentative de création d'un rectangle avec une hauteur négative.
         let _rect = Rectangle::new(10, -10);
     }
 }
