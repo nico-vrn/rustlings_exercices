013849db9885b53db6c5e962226320589170e286
nico-vrn <networksgamesni@gmail.com>
2024-04-23 16:09:26 +0200

    rustdocs

--- exercises/10_modules/modules3.rs
+++ exercises/10_modules/modules3.rs
@@ -8,10 +8,14 @@
 // Execute `rustlings hint modules3` or use the `hint` watch subcommand for a
 // hint.
 
+//! Ce programme affiche le nombre de secondes écoulées depuis le 1er janvier 1970 00:00:00 UTC.
+
 use std::time::{SystemTime, UNIX_EPOCH};
 
 fn main() {
+    /// Récupère le temps actuel sous forme de durée depuis l'époque UNIX.
     match SystemTime::now().duration_since(UNIX_EPOCH) {
+        /// Affiche le nombre de secondes écoulées depuis l'époque UNIX.
         Ok(n) => println!("1970-01-01 00:00:00 UTC was {} seconds ago!", n.as_secs()),
         Err(_) => panic!("SystemTime before UNIX EPOCH!"),
     }
