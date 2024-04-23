781a3f9e35d84372920febea8645214fdd5f5c19
nico-vrn <networksgamesni@gmail.com>
2024-04-23 16:07:15 +0200

    rustdocs

--- exercises/09_strings/strings4.rs
+++ exercises/09_strings/strings4.rs
@@ -7,24 +7,35 @@
 //
 // No hints this time!
 
+//! Ce programme illustre plusieurs façons de manipuler et d'afficher des chaînes de caractères en Rust.
 
+/// Affiche une référence à une chaîne de caractères.
 fn string_slice(arg: &str) {
     println!("{}", arg);
 }
 
+/// Affiche une chaîne de caractères propre.
 fn string(arg: String) {
     println!("{}", arg);
 }
 
 fn main() {
+    /// Affiche une référence à une chaîne de caractères littérale.
     string_slice("blue");
+    /// Affiche une chaîne de caractères créée à partir d'une chaîne littérale.
     string("red".to_string());
+    /// Affiche une chaîne de caractères créée à partir d'une chaîne existante.
     string(String::from("hi"));
-    string("rust is fun!".to_owned());
-    string("nice weather".into());
+    /// Affiche une chaîne de caractères créée à partir d'une chaîne littérale avec la méthode `into()`.
+    string("rust is fun!".into());
+    /// Affiche une chaîne de caractères formatée.
     string(format!("Interpolation {}", "Station"));
+    /// Affiche le premier caractère d'une chaîne de caractères référencée.
     string_slice(&String::from("abc")[0..1]);
+    /// Affiche une chaîne de caractères sans les espaces au début et à la fin.
     string("  hello there ".trim().to_string());
-    string("Happy Monday!".to_string().replace("Mon", "Tues"));
+    /// Affiche une chaîne de caractères avec un remplacement.
+    string("Happy Monday!".replace("Mon", "Tues"));
+    /// Affiche une chaîne de caractères convertie en minuscules.
     string("mY sHiFt KeY iS sTiCkY".to_lowercase());
 }
