ba9bcd75a4448a9b38278400e1bcce556608ba1d
nico-vrn <networksgamesni@gmail.com>
2024-04-23 15:57:03 +0200

    rustdocs4

--- exercises/04_primitive_types/primitive_types6.rs
+++ exercises/04_primitive_types/primitive_types6.rs
@@ -6,11 +6,18 @@
 // Execute `rustlings hint primitive_types6` or use the `hint` watch subcommand
 // for a hint.
 
+/// Cette fonction de test `indexing_tuple` vérifie l'indexation d'un tuple pour obtenir son deuxième élément.
+/// 
+/// Elle crée un tuple de nombres (1, 2, 3) et utilise l'indexation du tuple pour obtenir le deuxième élément, qui devrait être égal à 2.
+/// 
+/// # Remarque
+/// 
+/// Si l'indexation échoue et que le deuxième élément du tuple n'est pas égal à 2, un message d'erreur est affiché.
+
 
 #[test]
 fn indexing_tuple() {
     let numbers = (1, 2, 3);
-    // Utilisation de l'indexation du tuple pour obtenir le deuxième élément
     let second = numbers.1;
 
     assert_eq!(2, second,
