f6473078f19b01a4c97bbfa758788eaeb1ac91b3
nico-vrn <networksgamesni@gmail.com>
2024-04-23 16:15:08 +0200

    rustdocs

--- exercises/16_lifetimes/lifetimes3.rs
+++ exercises/16_lifetimes/lifetimes3.rs
@@ -5,10 +5,14 @@
 // Execute `rustlings hint lifetimes3` or use the `hint` watch subcommand for a
 // hint.
 
-// Définition de la structure `Book` avec des champs `author` et `title`.
+//! Ce programme définit une structure `Book` qui représente un livre avec un auteur et un titre.
+//! Il crée une instance de `Book` en utilisant des chaînes de caractères pour l'auteur et le titre.
+//! il affiche le titre et l'auteur du livre créé.
+
+/// Représente un livre avec un auteur et un titre.
 struct Book {
-    author: String, // L'auteur du livre.
-    title: String,  // Le titre du livre.
+    author: String, 
+    title: String,  
 }
 
 fn main() {
@@ -21,3 +25,4 @@ fn main() {
     // Affichage du titre et de l'auteur du livre.
     println!("{} by {}", book.title, book.author);
 }
+
