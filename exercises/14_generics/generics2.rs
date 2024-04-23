b67d60dc7fb81323b2fd20c88814e9edbc32fe4e
nico-vrn <networksgamesni@gmail.com>
2024-04-23 16:14:03 +0200

    rustdocs

--- exercises/14_generics/generics2.rs
+++ exercises/14_generics/generics2.rs
@@ -6,11 +6,16 @@
 // Execute `rustlings hint generics2` or use the `hint` watch subcommand for a
 // hint.
 
+//! Ce programme définit une structure générique `Wrapper<T>` qui enveloppe une valeur de n'importe quel type `T`.
+//! La méthode `new` permet de créer une nouvelle instance de `Wrapper`.
+
+/// Une structure générique qui enveloppe une valeur de type `T`.
 struct Wrapper<T> {
     value: T,
 }
 
 impl<T> Wrapper<T> {
+    /// Crée une nouvelle instance de `Wrapper` avec la valeur spécifiée.
     pub fn new(value: T) -> Self {
         Wrapper { value }
     }
@@ -22,11 +27,13 @@ mod tests {
 
     #[test]
     fn store_u32_in_wrapper() {
+        // Teste l'enveloppement d'un entier 32 bits non signé.
         assert_eq!(Wrapper::new(42).value, 42);
     }
 
     #[test]
     fn store_str_in_wrapper() {
+        // Teste l'enveloppement d'une chaîne de caractères.
         assert_eq!(Wrapper::new("Foo").value, "Foo");
     }
 }
--- exercises/15_traits/traits5.rs
+++ exercises/15_traits/traits5.rs
@@ -7,20 +7,29 @@
 // Execute `rustlings hint traits5` or use the `hint` watch subcommand for a
 // hint.
 
+//! Ce programme définit deux traits, `SomeTrait` et `OtherTrait`, avec une méthode par défaut qui renvoie toujours `true`.
+//! Il définit également deux structures, `SomeStruct` et `OtherStruct`, et implémente les deux traits pour chacune d'entre elles.
+//! La fonction `some_func` prend un argument de type générique `T` qui doit implémenter à la fois `SomeTrait` et `OtherTrait`.
+//! La fonction vérifie si les méthodes `some_function` et `other_function` renvoient toutes deux `true` pour l'élément donné.
 
+/// Un trait avec une méthode par défaut qui renvoie toujours `true`.
 pub trait SomeTrait {
     fn some_function(&self) -> bool {
         true
     }
 }
 
+/// Un autre trait avec une méthode par défaut qui renvoie toujours `true`.
 pub trait OtherTrait {
     fn other_function(&self) -> bool {
         true
     }
 }
 
+/// Une structure vide.
 struct SomeStruct {}
+
+/// Une autre structure vide.
 struct OtherStruct {}
 
 impl SomeTrait for SomeStruct {}
@@ -28,6 +37,7 @@ impl OtherTrait for SomeStruct {}
 impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
+/// Une fonction qui vérifie si les méthodes `some_function` et `other_function` renvoient toutes deux `true`.
 fn some_func<T>(item: T) -> bool
 where
     T: SomeTrait + OtherTrait,
@@ -36,6 +46,9 @@ where
 }
 
 fn main() {
+    // Appel de `some_func` avec une instance de `SomeStruct`.
     some_func(SomeStruct {});
+
+    // Appel de `some_func` avec une instance de `OtherStruct`.
     some_func(OtherStruct {});
 }
