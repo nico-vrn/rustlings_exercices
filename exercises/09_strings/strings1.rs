56a15b7fd06862e0ae10d8de157271282c8978ce
nico-vrn <networksgamesni@gmail.com>
2024-04-23 14:02:42 +0200

    exo string

--- exercises/09_strings/strings1.rs
+++ exercises/09_strings/strings1.rs
@@ -13,5 +13,6 @@ fn main() {
 }
 
 fn current_favorite_color() -> String {
-    "blue"
+    String::from("blue")
 }
+
--- exercises/09_strings/strings2.rs
+++ exercises/09_strings/strings2.rs
@@ -9,7 +9,7 @@
 
 fn main() {
     let word = String::from("green"); // Try not changing this line :)
-    if is_a_color_word(word) {
+    if is_a_color_word(&word) {
         println!("That is a color word I know!");
     } else {
         println!("That is not a color word I know.");
@@ -19,3 +19,4 @@ fn main() {
 fn is_a_color_word(attempt: &str) -> bool {
     attempt == "green" || attempt == "blue" || attempt == "red"
 }
+
