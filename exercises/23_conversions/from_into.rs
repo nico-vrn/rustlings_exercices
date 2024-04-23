8098235a2537e98793325bdd087cdfe1fbdcf05e
nico-vrn <networksgamesni@gmail.com>
2024-04-23 15:04:13 +0200

    exo conversions

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -41,19 +41,27 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
-    fn from(s: &str) -> Person {}
-}
+    fn from(s: &str) -> Person {
+        if s.is_empty() {
+            return Person::default();
+        }
 
-fn main() {
-    // Use the `from` function
-    let p1 = Person::from("Mark,20");
-    // Since From is implemented for Person, we should be able to use Into
-    let p2: Person = "Gerald,70".into();
-    println!("{:?}", p1);
-    println!("{:?}", p2);
+        let parts: Vec<&str> = s.split(',').collect();
+        if parts.len() != 2 {
+            return Person::default();
+        }
+
+        let name = parts[0];
+        if name.is_empty() {
+            return Person::default();
+        }
+
+        match parts[1].parse::<usize>() {
+            Ok(age) => Person { name: String::from(name), age },
+            Err(_) => Person::default(),
+        }
+    }
 }
 
 #[cfg(test)]
