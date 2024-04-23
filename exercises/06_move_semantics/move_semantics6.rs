7adb084f23aa5c154765fe496069bfdaffc275ec
nico-vrn <networksgamesni@gmail.com>
2024-04-23 16:00:56 +0200

    rustdoc

--- exercises/06_move_semantics/move_semantics6.rs
+++ exercises/06_move_semantics/move_semantics6.rs
@@ -5,6 +5,16 @@
 // Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand
 // for a hint.
 
+//! Ce programme prend en entrée une chaîne de caractères, extrait le dernier caractère de cette chaîne et l'affiche, puis convertit la chaîne en majuscules et l'affiche également.
+//!
+//! # Fonctions
+//!
+//! ## get_char
+//! La fonction `get_char` prend une chaîne de caractères en propriété, extrait le dernier caractère de cette chaîne et le retourne.
+//!
+//! ## string_uppercase
+//! La fonction `string_uppercase` prend une référence à une chaîne de caractères, la convertit en majuscules et retourne le résultat.
+//!
 
 fn main() {
     let data = "Rust is great!".to_string();
