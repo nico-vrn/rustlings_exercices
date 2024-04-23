13da3a31cd8cbc966677159f257e9267acd2ef1d
nico-vrn <networksgamesni@gmail.com>
2024-04-23 15:45:04 +0200

    rustdocs1

--- exercises/00_intro/intro2.rs
+++ exercises/00_intro/intro2.rs
@@ -5,6 +5,7 @@
 // Execute `rustlings hint intro2` or use the `hint` watch subcommand for a
 // hint.
 
+/// This function simply prints "Hello there!" to the console when the program is run.
 fn main() {
     println!("Hello there!");
 }
